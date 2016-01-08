2d941016f5ad59fb93f456e5263b10dba3814415
Anthony Torrero Collins <atc@securegravity.com>
2016-01-08 13:23:54 -0800

    Initial Commit

---
+++ calc-simple-assignment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  calc-simple-assignment
+//
+//  Created by Anthony Torrero Collins on 1/8/16.
+//  Copyright © 2016 Anthony Torrero Collins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
